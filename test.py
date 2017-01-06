d49016e472af6cd10ea9dbcd9a2fea6c515371f9
Rodrigo Luzuriaga <rodrigoluzuriaga@gmail.com>
2017-01-06 20:00:20 +0000

    Cleaned up some of the code that I already wrote so that it is not redundant. Leaveing old code as block comments for future reference of code I have done so that I don't do that again. Still need to fix the overwriting issue.

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 '''
 Author: Rodrigo Luzuriaga
 Date started: 01/04/2017
-Date last modified: 01/04/2017
+Date last modified: 01/06/2017
 
 This script was created to clean up an external hard drive with data scattered all over the place.
 The user, in this file, specifies the directory to where all the files are located and then creates folders for the
@@ -12,14 +12,13 @@ The program then copies or moves the files into the new directory that the progr
 import os
 import shutil
 
-search_directory = '/home/pi/Desktop'
+search_directory = '/home/ubuntu/workspace/testdir'
 
-clean_directory = '/home/pi/clean/'
+clean_directory = '/home/ubuntu/workspace/clean/'
 
-map_file_path = "/home/pi/clean/map.txt"
+map_file_path = "/home/ubuntu/workspace/clean/map.txt"
 
 map_file_write = open(map_file_path, 'w')
-
 
 for folderName, subFolders, fileNames in os.walk(search_directory):
     for fileName in fileNames:
@@ -33,33 +32,55 @@ map_file_write.close()
 
 map_file_read = open(map_file_path, 'r')
 
-for line in map_file_read:
-    # TODO: Need to make sure that if there are multiple files with the same name that they get changed so that it doesn't overwrite the files
-    if line[-3] == ".":
-        if not os.path.exists(clean_directory + line[-2:].strip("\n")):
-            os.makedirs(clean_directory + line[-2:].strip("\n"))
-            shutil.copy(line.strip("\n"), clean_directory + line[-2:].strip("\n"))
-        else:
-            shutil.copy(line.strip("\n"), clean_directory + line[-2:].strip("\n"))
-    
-    if line[-4] == ".":
-        if not os.path.exists(clean_directory + line[-3:].strips("\n")):
-            os.makedirs(clean_directory + line[-3:].strip("\n"))
-            shutil.copy(line.strip("\n"), clean_directory + line[-3:].strip("\n"))
-        else:
-            shutil.copy(line.strip("\n"), clean_directory + line[-3:].strip("\n"))
-    
-    if line[-5] == ".":
-        if not os.path.exists(clean_directory + line[-4:].strip("\n")):
-            os.makedirs(clean_directory + line[-4:].strip("\n"))
-            shutil.copy(line.strip("\n"), clean_directory + line[-4:].strip("\n"))
-        else:
-            shutil.copy(line.strip("\n"), clean_directory + line[-4:].strip("\n"))
 
+for line in map_file_read:
+    line = line.strip("\n")
+# TODO: Need to make sure that if there are multiple files with the same name that they get changed so that it doesn't overwrite the files
+    if line[-3] == ".":
+        destination_path = clean_directory + line[-2:]
+        
+    elif line[-4] == ".":
+        destination_path = clean_directory + line[-3:]
+    elif line[-5] == ".":
+        destination_path = clean_directory + line[-4:]
+    
+    if not os.path.exists(destination_path):
+        os.makedirs(destination_path)
+    
+    shutil.copy2(line, destination_path)
+    
 
 map_file_read.close()
 
+
+
+
 """
+for line in map_file_read:
+    line = line.strip("\n")
+# TODO: Need to make sure that if there are multiple files with the same name that they get changed so that it doesn't overwrite the files
+    if line[-3] == ".":
+        if not os.path.exists(clean_directory + line[-2:]):
+            os.makedirs(clean_directory + line[-2:])
+            shutil.copy(line, clean_directory + line[-2:])
+        else:
+            shutil.copy(line, clean_directory + line[-2:])
+    
+    if line[-4] == ".":
+        if not os.path.exists(clean_directory + line[-3:]):
+            os.makedirs(clean_directory + line[-3:])
+            shutil.copy(line, clean_directory + line[-3:])
+        else:
+            shutil.copy(line, clean_directory + line[-3:])
+    
+    if line[-5] == ".":
+        if not os.path.exists(clean_directory + line[-4:]):
+            os.makedirs(clean_directory + line[-4:])
+            shutil.copy(line, clean_directory + line[-4:])
+        else:
+            shutil.copy(line, clean_directory + line[-4:])
+
+
 for folderName, subFolders, fileNames in os.walk('/home/pi/Desktop/'):
 	for subFolder in subFolders:
 		for fileName in fileNames:
