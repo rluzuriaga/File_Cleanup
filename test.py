dc6a8989d567a81121aeb3bf2298020042b4636e
Rodrigo Luzuriaga <rodrigoluzuriaga@gmail.com>
2017-01-25 15:37:42 +0000

    Completed the script! Leaving the commented old code of what I tried for this commit and will then delete it afterwards. Still need to add comments and test a few things out.

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 '''
 Author: Rodrigo Luzuriaga
 Date started: 01/04/2017
-Date last modified: 01/06/2017
+Date last modified: 01/25/2017
 
 This script was created to clean up an external hard drive with data scattered all over the place.
 The user, in this file, specifies the directory to where all the files are located and then creates folders for the
@@ -11,6 +11,7 @@ The program then copies or moves the files into the new directory that the progr
 
 import os
 import shutil
+from time import sleep
 
 search_directory = '/home/ubuntu/workspace/testdir'
 
@@ -18,7 +19,13 @@ clean_directory = '/home/ubuntu/workspace/clean/'
 
 map_file_path = "/home/ubuntu/workspace/clean/map.txt"
 
+
+full_name_list = []
+
+file_name_list = []
+
 map_file_write = open(map_file_path, 'w')
+file_names_write = open("/home/ubuntu/workspace/clean/fileNames.txt", 'w')
 
 for folderName, subFolders, fileNames in os.walk(search_directory):
     for fileName in fileNames:
@@ -26,31 +33,135 @@ for folderName, subFolders, fileNames in os.walk(search_directory):
             pass
         else:
             map_file_write.write(folderName + "/" + fileName + "\n")
+            file_names_write.write(fileName + "\n")
+            
+            # Append to two lists with folderName+fileName and just fileName
+            full_name_list.append(folderName + "/")
+            file_name_list.append(fileName)
+            
 
 map_file_write.close()
 
 
-map_file_read = open(map_file_path, 'r')
+class static:
+    number = 0
 
 
-for line in map_file_read:
-    line = line.strip("\n")
-# TODO: Need to make sure that if there are multiple files with the same name that they get changed so that it doesn't overwrite the files
-    if line[-3] == ".":
-        destination_path = clean_directory + line[-2:]
-        
-    elif line[-4] == ".":
-        destination_path = clean_directory + line[-3:]
-    elif line[-5] == ".":
-        destination_path = clean_directory + line[-4:]
+
+for full_path, file_name in zip(full_name_list, file_name_list):
+    i = 0
+    
+    if file_name[-3] == ".":
+        destination_path = clean_directory + file_name[-2:]
+    elif file_name[-4] == ".":
+        destination_path = clean_directory + file_name[-3:]
+    elif file_name[-5] == ".":
+        destination_path = clean_directory + file_name[-4:]
     
     if not os.path.exists(destination_path):
         os.makedirs(destination_path)
     
-    shutil.copy2(line, destination_path)
+    
+    def file_renamer(file_name, i):
+        file_name_no_ext = file_name.split(".", 1)[0]  # Creates the variable that only holds the file name without the file extension
+        file_name_ext = file_name.split(".", 1)[1]
+        new_file_name = file_name_no_ext + "(" + str(i) + ")" + "." + file_name_ext
+        i += 1
+        
+        
+        if os.path.exists(destination_path + "/" + new_file_name):
+            file_renamer(file_name, i)
+        
+        try:
+            shutil.move(destination_path + "/" + file_name, destination_path + "/" + new_file_name)
+        except IOError:
+            pass
+    
+    if os.path.exists(destination_path + "/" + file_name):
+        file_renamer(file_name, i)
+    
+    
+    shutil.copy2(full_path + file_name, destination_path)
+    
+    
+    # def recur(new_file_name, static_number):
+    #     # print(os.path.exists(destination_path + "/" +new_file_name))
+    #     # print(destination_path + "/" + new_file_name)
+    #     i = static_number
+    #     old_file_name = new_file_name
+    #     # print("destination_path + '/' + new_file_name:  " + destination_path + "/" + new_file_name)
+    #     if os.path.exists(destination_path + "/" + new_file_name):  # Just a second check for safe keeping
+    #         # print("yes, ", new_file_name)
+    #         file_name_no_ext = file_name.split(".", 1)[0]  # creates the variable that will contain only the file name with no extension
+    #         file_name_ext = file_name.split(".", 1)[1]  # Creates the variable that will contain only the file extension
+    #         new_file_name = file_name_no_ext + "(" + str(i) + ")" + "." + file_name_ext  # Creates the new file name with an addition of "(#)" where the # is a number starting with 1
+    #         i += 1
+            
+    #         # sleep(5)
+    #         shutil.move(destination_path + "/" + file_name, destination_path + "/" + new_file_name)
+    #         # print("Moving: " + destination_path + "/" + file_name + "  to:  " + destination_path + "/" + new_file_name + "\n\n")
+    #         # sleep(5)
+            
+    #         # shutil.move(full_path + file_name, full_path + new_file_name)
+    #         # print("Moving: " + full_path + file_name + "  to:  " + full_path + new_file_name + "\n\n")
+            
+    #         full_name_list.append(full_path)
+    #         # print("New full_name_list")
+    #         # print(full_name_list)
+    #         # print("\n\n")
+            
+    #         file_name_list.append(new_file_name)
+    #         # print("New file_name_list")
+    #         # print(file_name_list)
+    #         # print("\n\n")
+            
+    #         # sleep(5)
+    #         # print("Second if:  destination_path + '/' + old_file_name: " + destination_path + "/" + old_file_name + " == " + str(os.path.exists(destination_path + "/" + old_file_name)) +  "\n\n")
+    #         # sleep(5)
+            
+    #         # if os.path.exists(destination_path + "/" + old_file_name) and old_file_name != file_name:
+    #         #     recur(old_file_name, i)
+            
+    #         # pass
+
+    
+    # # recur(new_file_name, static.number)
+    
+    # if os.path.exists(destination_path + "/" + file_name):
+    #     recur(file_name, static.number)
+    
+    # print(new_file_name)
+    
+    # print("Copying  " + full_path + file_name + "  to  " + destination_path + "\n\n")
+    
+    
+    # try:
+    #     shutil.copy2(full_path + file_name, destination_path)
+    # except IOError as e:
+    #     pass
+
+
+
+# map_file_read = open(map_file_path, 'r')
+
+
+# for line in map_file_read:
+#     line = line.strip("\n")
+# # TODO: Need to make sure that if there are multiple files with the same name that they get changed so that it doesn't overwrite the files
+#     if line[-3] == ".":
+#         destination_path = clean_directory + line[-2:]
+#     elif line[-4] == ".":
+#         destination_path = clean_directory + line[-3:]
+#     elif line[-5] == ".":
+#         destination_path = clean_directory + line[-4:]
+    
+#     if not os.path.exists(destination_path):
+#         os.makedirs(destination_path)
+    
+#     shutil.copy2(line, destination_path)
     
 
-map_file_read.close()
+# map_file_read.close()
 
 
 
