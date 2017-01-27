6fe237a3f897796cf32ff185e3dba8cf31445029
rluzuriaga <rluzuriagaaguirre@student.mtsac.edu>
2017-01-27 15:51:34 -0800

    Added more comments and cleaned up just a little bit of code.

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 '''
 Author: Rodrigo Luzuriaga
 Date started: 01/04/2017
-Date last modified: 01/25/2017
+Date last modified: 01/27/2017
 
 This script was created to clean up an external hard drive with data scattered all over the place.
 The user, in this file, specifies the directory to where all the files are located and then creates folders for the
@@ -17,7 +17,7 @@ search_directory = '/home/ubuntu/workspace/testdir'  # This is the directory tha
 
 clean_directory = '/home/ubuntu/workspace/clean/'  # This is the directory that you want to move the files to. In this directory the script will create all the necesary folders and copy over the files
 
-map_file_path = "/home/ubuntu/workspace/clean/map.txt"
+map_file_path = "/home/ubuntu/workspace/clean/map.txt"  # TODO: Need to make this text file a file that shows where each file was moved from and to
 
 
 
@@ -32,7 +32,7 @@ map_file_path = "/home/ubuntu/workspace/clean/map.txt"
 ###########################################################################################################################################################################
 ###########################################################################################################################################################################
 ## 
-### DO NOT CHANGE ANYTHING BELLOW UNLESS YOU KNOW WHAT YOU ARE DOING
+### DO NOT CHANGE ANYTHING BELLOW, UNLESS YOU KNOW WHAT YOU ARE DOING
 ##
 ###########################################################################################################################################################################
 ###########################################################################################################################################################################
@@ -47,14 +47,14 @@ map_file_path = "/home/ubuntu/workspace/clean/map.txt"
 
 
 
-full_name_list = []
+full_name_list = [] 
 
 file_name_list = []
 
-map_file_write = open(map_file_path, 'w')
-file_names_write = open("/home/ubuntu/workspace/clean/fileNames.txt", 'w')
+map_file_write = open(map_file_path, 'w')  # Opening file that contains all the the information of where the file was moved from and to
+file_names_write = open("/home/ubuntu/workspace/clean/fileNames.txt", 'w')  # TODO: Need to check if this file would really be needed of if another file needs to be created
 
-for folderName, subFolders, fileNames in os.walk(search_directory):
+for folderName, subFolders, fileNames in os.walk(search_directory):  # 
     for fileName in fileNames:
         if folderName == clean_directory:
             pass
@@ -86,19 +86,22 @@ for full_path, file_name in zip(full_name_list, file_name_list):
     
     
     def file_renamer(file_name, i):
-        file_name_no_ext = file_name.split(".", 1)[0]  # Creates the variable that only holds the file name without the file extension
-        file_name_ext = file_name.split(".", 1)[1]
-        new_file_name = file_name_no_ext + "(" + str(i) + ")" + "." + file_name_ext
-        i += 1
+        file_name_no_ext = file_name.split(".", 1)[0]  # Creates variable that only holds the file name without the file extension
+        
+		file_name_ext = file_name.split(".", 1)[1]  # Creates variable that holds the file name extension
+        
+		new_file_name = file_name_no_ext + "(" + str(i) + ")" + "." + file_name_ext  # Creates the new file name that adds the file name without the extension and a number starting from 0 in paranthesis and then adds the the file extension after a dot
+        
+		i += 1
         
         
-        if os.path.exists(destination_path + "/" + new_file_name):
-            file_renamer(file_name, i)
+        if os.path.exists(destination_path + "/" + new_file_name):  # Checks if this new file name is already in the new directory
+            file_renamer(file_name, i)  # Runs the function again usinf the original file name and the new number that is incremented by one every time this function runs
         
         try:
             shutil.move(destination_path + "/" + file_name, destination_path + "/" + new_file_name)
-        except IOError:
-            pass
+        except IOError:  # The only time that the IOError exception should happen is when the shutil.move() function is trying to rename a file that is not there.
+            pass  # If the exteption does happen then the for loop ends
     
     if os.path.exists(destination_path + "/" + file_name):
         file_renamer(file_name, i)
