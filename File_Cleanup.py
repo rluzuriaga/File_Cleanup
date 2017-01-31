e20025e06cf408b85e47a4dce73933843a02f1ec
Rodrigo Luzuriaga <rodrigoluzuriaga@gmail.com>
2017-01-31 00:57:54 +0000

    Renamed test.py to File_Cleanup.py. Added code to determine if user want to move or copy the files. Removed redundant file creation that was used in previous tests, and the writes to the file as well. Changed some variables to non-generic names to make file be more readable. Added functionality to write to a file the source and destination of every file that is moved/copied. This still needs to some work but it is working if the script is run only once. Added more comments to make code more understandable.

--- File_Cleanup.py
+++ File_Cleanup.py
@@ -1,7 +1,7 @@
 '''
 Author: Rodrigo Luzuriaga
 Date started: 01/04/2017
-Date last modified: 01/27/2017
+Date last modified: 01/30/2017
 
 This script was created to clean up an external hard drive with data scattered all over the place.
 The user, in this file, specifies the directory to where all the files are located and then creates folders for the
@@ -18,6 +18,10 @@ search_directory = '/home/ubuntu/workspace/testdir'  # This is the directory tha
 clean_directory = '/home/ubuntu/workspace/clean/'  # This is the directory that you want to move the files to. In this directory the script will create all the necesary folders and copy over the files
 
 map_file_path = "/home/ubuntu/workspace/clean/map.txt"  # TODO: Need to make this text file a file that shows where each file was moved from and to
+
+remove_old_files_after_move = False  # For a lack of a better variable name, this just decides if the files are moved or copied
+                                    # Make this variable False if you want the files to be copied (meaning it keeps the original files in place)
+                                    # Make this variable True if you want the file to be moved (meaning they will be removed from the original place)
 
 
 
@@ -43,7 +47,7 @@ map_file_path = "/home/ubuntu/workspace/clean/map.txt"  # TODO: Need to make thi
 
 
 
-
+map_file_write = open(map_file_path, 'w')  # Opening file that contains all the the information of where the file was moved from and to
 
 
 
@@ -51,28 +55,20 @@ full_name_list = []
 
 file_name_list = []
 
-map_file_write = open(map_file_path, 'w')  # Opening file that contains all the the information of where the file was moved from and to
-file_names_write = open("/home/ubuntu/workspace/clean/fileNames.txt", 'w')  # TODO: Need to check if this file would really be needed of if another file needs to be created
 
-for folderName, subFolders, fileNames in os.walk(search_directory):  # 
+for folderName, subFolders, fileNames in os.walk(search_directory):  # The function os.walk() gives out three variables while it is "walking" through the directory
     for fileName in fileNames:
         if folderName == clean_directory:
             pass
         else:
-            map_file_write.write(folderName + "/" + fileName + "\n")
-            file_names_write.write(fileName + "\n")
-            
-            # Append to two lists with folderName+fileName and just fileName
+            # Append to two lists with folderName + fileName and just fileName
             full_name_list.append(folderName + "/")
             file_name_list.append(fileName)
-            
-
-map_file_write.close()
-
+ 
 
 
 for full_path, file_name in zip(full_name_list, file_name_list):
-    i = 0
+    counter = 0
     
     if file_name[-3] == ".":
         destination_path = clean_directory + file_name[-2:]
@@ -84,27 +80,45 @@ for full_path, file_name in zip(full_name_list, file_name_list):
     if not os.path.exists(destination_path):
         os.makedirs(destination_path)
     
-    
-    def file_renamer(file_name, i):
+    modified_file_name = ""
+    def file_renamer(file_name, counter):
+        
+        
         file_name_no_ext = file_name.split(".", 1)[0]  # Creates variable that only holds the file name without the file extension
         
-		file_name_ext = file_name.split(".", 1)[1]  # Creates variable that holds the file name extension
+        file_name_ext = file_name.split(".", 1)[1]  # Creates variable that holds the file name extension
         
-		new_file_name = file_name_no_ext + "(" + str(i) + ")" + "." + file_name_ext  # Creates the new file name that adds the file name without the extension and a number starting from 0 in paranthesis and then adds the the file extension after a dot
+        new_file_name = file_name_no_ext + "(" + str(counter) + ")" + "." + file_name_ext  # Creates the new file name that adds the file name without the extension and a number starting from 0 in paranthesis and then adds the the file extension after a dot
         
-		i += 1
+        counter += 1
         
         
         if os.path.exists(destination_path + "/" + new_file_name):  # Checks if this new file name is already in the new directory
-            file_renamer(file_name, i)  # Runs the function again usinf the original file name and the new number that is incremented by one every time this function runs
+            file_renamer(file_name, counter)  # Runs the function again usinf the original file name and the new number that is incremented by one every time this function runs
         
         try:
             shutil.move(destination_path + "/" + file_name, destination_path + "/" + new_file_name)
         except IOError:  # The only time that the IOError exception should happen is when the shutil.move() function is trying to rename a file that is not there.
-            pass  # If the exteption does happen then the for loop ends
+            pass  # If the exteption does happen then the current state of the for loop ends
+        
+        modified_file_name = new_file_name
+        return modified_file_name
+    
     
     if os.path.exists(destination_path + "/" + file_name):
-        file_renamer(file_name, i)
+        modified_file_name = file_renamer(file_name, counter)
     
+    if not remove_old_files_after_move:
+        shutil.copy2(full_path + file_name, destination_path)
+    elif remove_old_files_after_move:
+        shutil.move(full_path + file_name, destination_path)
+    else:
+        print("Please make the variable 'remove_old_files_after_move' either True or False.\n")
     
-    shutil.copy2(full_path + file_name, destination_path)
+    if modified_file_name == "":
+        modified_file_name = file_name
+
+    map_file_write.write("Moving:  " + full_path + file_name + "  --  To:  " + destination_path + "/" + modified_file_name +"\n\n")
+
+
+map_file_write.close()
