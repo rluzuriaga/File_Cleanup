0cd2f04609764a5ce6bc25dfbf45faca6959ed02
rluzuriaga <rluzuriagaaguirre@student.mtsac.edu>
2017-02-06 17:24:18 -0800

    Created a new file that will contain all the code after rewriting the original File_Cleanup code. This file will be object oriented.

---
+++ New_File_Cleanup.py
@@ -0,0 +1,10 @@
+"""
+Author: Rodrigo Luzuriaga
+Date Started: 01/04/2017
+Date last modified: 02/06/2017
+
+This script was created to clean up an external hard drive with data scattered all over the place.
+The user, in this file, specifies the directory to where all the files are located and then creates folders for the
+	file extension of all the files in the directory and all the sub-directories.
+The program then copies or moves the files into the new directory that the program made for those specific files.
+"""
