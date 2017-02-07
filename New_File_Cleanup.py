67346bf50bbd945a5cb14ce2a81d1a498b8b2104
rluzuriaga <rluzuriagaaguirre@student.mtsac.edu>
2017-02-06 21:03:55 -0800

    Started working on the new file and added two functions. Looking into correct docstring documentation.

--- New_File_Cleanup.py
+++ New_File_Cleanup.py
@@ -8,3 +8,108 @@ The user, in this file, specifies the directory to where all the files are locat
 	file extension of all the files in the directory and all the sub-directories.
 The program then copies or moves the files into the new directory that the program made for those specific files.
 """
+
+import os
+import shutil
+
+
+def lists_creation(file_path, search_directory, list_of_original_path, list_of_file_names):
+	"""
+
+	:param file_path:
+	:param search_directory:
+	:param list_of_original_path:
+	:param list_of_file_names:
+	:return:
+	"""
+
+	map_file_write = open(file_path, 'w')
+
+	for folderName, subFolder, fileNames in os.walk(search_directory):
+		for fileName in fileNames:
+			if folderName in fileNames:
+				pass
+			else:
+				list_of_original_path.append(folderName + "/")
+				list_of_file_names.append(fileName)
+
+	return map_file_write
+
+
+def destination_path_creation(file_name, clean_directory):
+	"""
+
+	:param file_name:
+	:param clean_directory:
+	:return:
+	"""
+
+	if file_name[-3] == ".":
+		destination_path = clean_directory + file_name[-2:]
+	elif file_name[-4] == ".":
+		destination_path = clean_directory + file_name[-3:]
+	elif file_name[-5] == ".":
+		destination_path = clean_directory + file_name[-4:]
+
+	if not os.path.exists(destination_path):
+		os.makedirs(destination_path)
+
+	return destination_path
+
+
+"""
+def file_renamer(file_name, counter, m_f_n):
+	r_m_f_n = ""  # Since this function is recursive, this variable has to be created to be the output of the recursive part of the function
+
+	file_name_no_ext = file_name.split(".", 1)[0]  # Creates variable that only holds the file name without the file extension
+
+	file_name_ext = file_name.split(".", 1)[1]  # Creates variable that holds the file name extension
+
+	new_file_name = file_name_no_ext + "(" + str(counter) + ")" + "." + file_name_ext  # Creates the new file name that adds the file name without the extension and a number starting from 0 in paranthesis and then adds the the file extension after a dot
+
+	counter += 1
+
+
+	if os.path.exists(destination_path + "/" + new_file_name):  # Checks if this new file name is already in the new directory
+
+		r_m_f_n = file_renamer(file_name, counter, m_f_n)  # Runs the function again using the original file name and the new number that is incremented by one every time this function runs
+															# If this function runs (inside the actual function) then it outputs a variable that is checked bellow
+
+	try:
+		shutil.move(destination_path + "/" + file_name, destination_path + "/" + new_file_name)
+	except IOError:  # The only time that the IOError exception should happen is when the shutil.move() function is trying to rename a file that is not there.
+		pass  # If the exception does happen, then the current state of the for loop ends
+
+	if r_m_f_n == "":  # Checks if the function only ran once, if it ran once then the variable is an empty string
+		m_f_n = new_file_name  # When the function only runs once then the variable that will be returned will be the new_file_name
+	else:  # If the the function is ran more than once then the output of the second (or above) time the function is ran will be returned
+		m_f_n = r_m_f_n
+
+	return m_f_n
+"""
+
+
+
+if __name__ == "__main__":
+
+	search_directory = 'C:/Users/RodrigoLuzuriaga/Desktop/aaa/'  # If working on Windows, insert directory location like this line
+	# search_directory = '/home/ubuntu/workspace/testdir'  # This is the directory that you want to read from. Only change what is inside the ' '
+
+	clean_directory = 'C:/Users/RodrigoLuzuriaga/Desktop/bbb/'
+	# clean_directory = '/home/ubuntu/workspace/clean/'  # This is the directory that you want to move the files to. In this directory the script will create all the necesary folders and copy over the files
+
+	map_file_path = 'C:/Users/RodrigoLuzuriaga/Desktop/map.txt'
+	# map_file_path = "/home/ubuntu/workspace/clean/map.txt"
+
+	remove_old_files_after_move = False  # For a lack of a better variable name, this just decides if the files are moved or copied
+										# Make this variable False if you want the files to be copied (meaning it keeps the original files in place)
+										# Make this variable True if you want the file to be moved (meaning they will be removed from the original place)
+
+	full_name_list = []
+
+	file_name_list = []
+
+	map_file_write = lists_creation(map_file_path, search_directory, full_name_list, file_name_list)
+
+	for full_path, file_name in zip(full_name_list, file_name_list):
+		counter = 1
