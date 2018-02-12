50e74425c3bac1b63af7d9d25005e5812ac9fda7
rluzuriaga <rluzuriagaaguirre@student.mtsac.edu>
2018-02-11 21:34:57 -0800

    Created a new script called Backup.py. Script will be used to copy files over for work.

---
+++ Backup.py
@@ -0,0 +1,15 @@
+"""
+Author: Rodrigo Luzuriaga
+Date Started: 02/11/2018
+Date Last Modified: 02/11/2018
+
+This script is going to be based on the File_Cleanup script but is going to be used to copy files over from any hard drive at work.
+"""
+
+# Constants
+SEARCH_DIRECTORY = ''
+CLEAN_DIRECTORY = ''
+MAP_FILE = ''
+
+if __name__ == "__main__":
+    pass
